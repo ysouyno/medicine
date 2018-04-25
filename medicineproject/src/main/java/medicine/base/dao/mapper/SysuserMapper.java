9c9571868832fbfb7b5a741a983f7d2ae50e21aa
ysouyno <ysouyno@163.com>
2018-04-25 22:46:53 +0800

    the file generated by MyBatisGeneratorSqlMap is tested successfully

---
+++ medicineproject/src/main/java/medicine/base/dao/mapper/SysuserMapper.java
@@ -0,0 +1,30 @@
+package medicine.base.dao.mapper;
+
+import java.util.List;
+import medicine.base.pojo.po.Sysuser;
+import medicine.base.pojo.po.SysuserExample;
+import org.apache.ibatis.annotations.Param;
+
+public interface SysuserMapper {
+    int countByExample(SysuserExample example);
+
+    int deleteByExample(SysuserExample example);
+
+    int deleteByPrimaryKey(String id);
+
+    int insert(Sysuser record);
+
+    int insertSelective(Sysuser record);
+
+    List<Sysuser> selectByExample(SysuserExample example);
+
+    Sysuser selectByPrimaryKey(String id);
+
+    int updateByExampleSelective(@Param("record") Sysuser record, @Param("example") SysuserExample example);
+
+    int updateByExample(@Param("record") Sysuser record, @Param("example") SysuserExample example);
+
+    int updateByPrimaryKeySelective(Sysuser record);
+
+    int updateByPrimaryKey(Sysuser record);
+}
---
+++ medicineproject/src/test/java/medicine/base/dao/mapper/SysuserMapperTest.java
@@ -0,0 +1,73 @@
+package medicine.base.dao.mapper;
+
+import java.util.List;
+
+import medicine.base.pojo.po.Sysuser;
+import medicine.base.pojo.po.SysuserExample;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import junit.framework.TestCase;
+
+public class SysuserMapperTest extends TestCase {
+	private ApplicationContext applicationContext;
+	private SysuserMapper sysuserMapper;
+
+	protected void setUp() throws Exception {
+		applicationContext = new ClassPathXmlApplicationContext(new String[] {
+				"spring/applicationContext.xml",
+				"spring/applicationContext-dao.xml" });
+
+		sysuserMapper = (SysuserMapper) applicationContext
+				.getBean("sysuserMapper");
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	public void testDeleteByPrimaryKey() {
+		fail("Not yet implemented");
+	}
+
+	public void testInsert() {
+		fail("Not yet implemented");
+	}
+
+	public void testSelectByExample() {
+		SysuserExample sysuserExample = new SysuserExample();
+		SysuserExample.Criteria criteria = sysuserExample.createCriteria();
+		criteria.andUseridEqualTo("gcxarzc");
+		criteria.andGroupidEqualTo("3");
+		List<Sysuser> sysuser = sysuserMapper.selectByExample(sysuserExample);
+		System.out.println(sysuser);
+	}
+
+	public void testSelectByPrimaryKey() {
+		Sysuser sysuser = sysuserMapper.selectByPrimaryKey("286");
+		System.out.println(sysuser);
+	}
+
+	// 传入的PO类的属性不为空才更新
+	public void testUpdateByPrimaryKeySelective() {
+		// 定义一个新对象
+		Sysuser sysuser = new Sysuser();
+		// 设置对象id
+		sysuser.setId("286");
+		// 设置需要更新的值
+		sysuser.setUsername("test_username0");
+		// 执行更新
+		sysuserMapper.updateByPrimaryKeySelective(sysuser);
+	}
+
+	// 不管传入的PO类的属性是否为空都更新
+	public void testUpdateByPrimaryKey() {
+		// 先查询对象
+		Sysuser sysuser = sysuserMapper.selectByPrimaryKey("286");
+		// 再向对象中设置值
+		sysuser.setUsername("test_username1");
+		// 最后执行更新
+		sysuserMapper.updateByPrimaryKey(sysuser);
+	}
+}
